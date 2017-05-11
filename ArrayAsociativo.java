7987584cb8939a996dd721b66b3b4fd9509fa0cb
juanpb-ser13 <juanpalmaborda@hotmail.com>
2017-05-11 12:19:17 +0200

    Size prueba2

--- ArrayAsociativo.java
+++ ArrayAsociativo.java
@@ -35,7 +35,7 @@ public class ArrayAsociativo {
 	}
 
 	public int size() {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 	
--- Tests.java
+++ Tests.java
@@ -8,11 +8,20 @@ import junit.framework.Assert;
 
 public class Tests {
 	private ArrayAsociativo array;
+	private String claves[]=new String[10];
+	private String valores[]=new String[10];
 	@Test
 	public void testsizevacio0() {
 		array=new ArrayAsociativo();
 		int i=array.size();
 		assertEquals(new Integer(0), new Integer(i));
 	}
-
+	@Test
+	public void testsizenovacio0() {
+		claves[0]="Hola";
+		valores[0]="Adios";
+		array=new ArrayAsociativo(claves,valores);
+		int i=array.size();
+		assertEquals(new Integer(1), new Integer(i));
+	}
 }
